76409964d8f29ce4eef233ed1f7003e10fe6ee45
Knitschi <knitschi@freenet.de>
2018-02-17 13:42:02 +0100

    Add basic files

---
+++ a.cpp
@@ -0,0 +1,11 @@
+#include <app2A/mainImpl.h>
+
+#include <lib1/lib1.h>
+
+int mainImpl(int argc, char **argv)
+{
+    (void) argc;
+    (void) argv;
+    lb1::lib1Function();
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+
+#include <app2A/mainImpl.h>
+
+int main(int argc, char **argv)
+{
+    return mainImpl(argc, argv);
+}
