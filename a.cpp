1feb8b82672583c9f9d532f1363d14229d836ca4
knitschi <knitschi@freenet.de>
2018-02-23 16:06:58 +0100

    use all packages

--- a.cpp
+++ a.cpp
@@ -1,6 +1,10 @@
 #include <APackage/a.h>
 #include <BPackage/b.h>
 #include <CPackage/c.h>
+#include <DPackage/d.h>
+#include <EPackage/e.h>
+#include <FPackage/f.h>
+#include <GPackage/g.h>
 
 int mainImpl(int argc, char **argv)
 {
@@ -8,6 +12,11 @@ int mainImpl(int argc, char **argv)
     (void) argv;
     b::bFunction();
     c::cFunction();
+    d::dFunction();
+    e::eFunction();
+    f::fFunction();
+    g::gFunction();
+
     return 0;
 }
 
