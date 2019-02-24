c20a22e429e5ab290e6518fe2366c3f9b2afe430
Knitschi <knitschi@freenet.de>
2019-02-24 16:20:52 +0100

    Adds some code that can be formated.

--- function.h
+++ function.h
@@ -3,3 +3,26 @@
 #include <APackage/a_export.h>
 
 A_EXPORT int mainImpl(int argc, char **argv);
+
+namespace a {
+
+class A_EXPORT Blub : std::vector<int> {
+public:
+  void Blub(int a) : std::vector<int>(3), m_a{1}, m_b{2} { double d = 0; }
+
+  void blub(int a, int b, int c, int d, int e, int f) {
+    for (int i = 0; i < 5; i++) {
+      if (i == 3) {
+        m_a = 0;
+      } else {
+        m_b = 0;
+      }
+    }
+  }
+
+private:
+  int m_a = 0;
+  int m_b = 0;
+}
+
+} // namespace a
